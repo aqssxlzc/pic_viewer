572533722631fa80989ba511f6440db62900f2c1
Chen <aqssxlzc@gmail.com>
2026-03-05 15:47:07 +0800

    可以 牛而

--- imageviewer.cpp
+++ imageviewer.cpp
@@ -1,7 +1,5 @@
 #include "imageviewer.h"
 #include <QVBoxLayout>
-#include <QHBoxLayout>
-#include <QHBoxLayout>
 #include <QImageReader>
 #include <QScreen>
 #include <QApplication>
@@ -9,42 +7,82 @@
 #include <QPropertyAnimation>
 #include <QMediaContent>
 #include <QFileInfo>
+#include <QtConcurrent>
 
 ImageViewer::ImageViewer(const QString &filePath, const QStringList &allPaths, QWidget *parent)
     : QDialog(parent)
     , mediaPaths(allPaths)
     , currentIndex(allPaths.indexOf(filePath))
     , isVideo(false)
+    , currentLabelIndex(0)
+    , videoPlayTimer(nullptr)
 {
     setupUI();
     loadMedia();
-    
+
     setWindowState(Qt::WindowFullScreen);
     setModal(true);
+}
+
+ImageViewer::~ImageViewer()
+{
+    // 取消所有异步加载任务
+    for (auto watcher : preloadWatchers) {
+        if (watcher) {
+            watcher->cancel();
+            disconnect(watcher, nullptr, nullptr, nullptr);
+            if (!watcher->isFinished()) {
+                watcher->waitForFinished();
+            }
+            delete watcher;
+        }
+    }
+    preloadWatchers.clear();
+    pixmapCache.clear();
+
+    if (videoPlayTimer) {
+        videoPlayTimer->stop();
+        delete videoPlayTimer;
+    }
 }
 
 void ImageViewer::setupUI()
 {
     setStyleSheet("background-color: rgba(0, 0, 0, 240);");
     setWindowFlags(Qt::Dialog | Qt::FramelessWindowHint);
-    
+
     QVBoxLayout *mainLayout = new QVBoxLayout(this);
     mainLayout->setContentsMargins(0, 0, 0, 0);
 
-    // Image label
-    imageLabel = new QLabel(this);
-    imageLabel->setAlignment(Qt::AlignCenter);
-    imageLabel->setScaledContents(false);
-    mainLayout->addWidget(imageLabel);
-    
+    // 使用 QStackedWidget 实现双缓冲，避免黑屏
+    imageStack = new QStackedWidget(this);
+    mainLayout->addWidget(imageStack);
+
+    // 创建多个 QLabel 用于双缓冲
+    for (int i = 0; i < 3; ++i) {
+        QLabel *label = new QLabel(imageStack);
+        label->setAlignment(Qt::AlignCenter);
+        label->setScaledContents(false);
+        label->setStyleSheet("background-color: transparent;");
+        imageLabels.append(label);
+        imageStack->addWidget(label);
+    }
+    imageStack->setCurrentIndex(0);
+
     // Video widget
     videoWidget = new QVideoWidget(this);
+    videoWidget->setStyleSheet("background-color: black;");
     videoWidget->hide();
     mainLayout->addWidget(videoWidget);
-    
-    // Media player
+
+    // Media player - 只创建一个实例
     mediaPlayer = new QMediaPlayer(this, QMediaPlayer::VideoSurface);
     mediaPlayer->setVideoOutput(videoWidget);
+
+    // 视频延迟播放定时器
+    videoPlayTimer = new QTimer(this);
+    videoPlayTimer->setSingleShot(true);
+    connect(videoPlayTimer, &QTimer::timeout, this, &ImageViewer::playPendingVideo);
 
     // Close button
     closeButton = new QPushButton("✕", this);
@@ -99,7 +137,7 @@ void ImageViewer::setupUI()
     );
     nextButton->move(width() - 80, height() / 2 - 30);
     connect(nextButton, &QPushButton::clicked, this, &ImageViewer::showNext);
-    
+
     // Play/Pause button for videos
     playPauseButton = new QPushButton("⏸", this);
     playPauseButton->setFixedSize(60, 60);
@@ -130,7 +168,7 @@ void ImageViewer::setupUI()
     // Add fade-in animation
     QGraphicsOpacityEffect *effect = new QGraphicsOpacityEffect(this);
     setGraphicsEffect(effect);
-    
+
     QPropertyAnimation *animation = new QPropertyAnimation(effect, "opacity");
     animation->setDuration(300);
     animation->setStartValue(0.0);
@@ -146,60 +184,108 @@ void ImageViewer::loadMedia()
     }
 
     QString filePath = mediaPaths[currentIndex];
+    bool wasVideo = isVideo;
     isVideo = isVideoFile(filePath);
-    
+
+    // 停止任何待播放的视频
+    videoPlayTimer->stop();
+    pendingVideoPath.clear();
+
+    // 先停止视频播放并清理资源
+    if (wasVideo || isVideo) {
+        mediaPlayer->stop();
+        // 清除当前媒体源，释放 GStreamer 资源
+        mediaPlayer->setMedia(QMediaContent());
+    }
+
     if (isVideo) {
-        // Stop current video if playing
-        mediaPlayer->stop();
-        
-        // Show video widget, hide image label
-        imageLabel->hide();
-        videoWidget->show();
-        playPauseButton->show();
-        
-        // Load and play video
-        mediaPlayer->setMedia(QMediaContent(QUrl::fromLocalFile(filePath)));
-        
-        // Loop video
-        connect(mediaPlayer, &QMediaPlayer::mediaStatusChanged, [this](QMediaPlayer::MediaStatus status) {
-            if (status == QMediaPlayer::EndOfMedia) {
-                mediaPlayer->setPosition(0);
-                mediaPlayer->play();
-            }
-        });
-        
-        mediaPlayer->play();
-        playPauseButton->setText("⏸");
-        updateVideo();
+        displayVideo(currentIndex);
     } else {
-        // Stop video if playing
-        mediaPlayer->stop();
-        
-        // Show image label, hide video widget
-        videoWidget->hide();
-        imageLabel->show();
-        playPauseButton->hide();
-        
-        // Load image
-        QImageReader reader(filePath);
-        reader.setAutoTransform(true);
-        
-        QImage image = reader.read();
-        if (!image.isNull()) {
-            currentPixmap = QPixmap::fromImage(image);
-            updateImage();
-        }
+        displayImage(currentIndex);
+        // 预加载相邻图片
+        preloadAdjacent();
     }
 
     QString fileType = isVideo ? "视频" : "图片";
     setWindowTitle(QString("%1查看器 - %2/%3").arg(fileType).arg(currentIndex + 1).arg(mediaPaths.size()));
 }
 
+void ImageViewer::displayImage(int index)
+{
+    // Show image stack, hide video widget
+    videoWidget->hide();
+    playPauseButton->hide();
+    imageStack->show();
+
+    QString filePath = mediaPaths[index];
+
+    // 检查缓存中是否有预加载的图片
+    {
+        QMutexLocker locker(&cacheMutex);
+        if (pixmapCache.contains(index)) {
+            QPixmap pixmap = pixmapCache[index];
+            QLabel *label = getAvailableLabel();
+            label->setPixmap(pixmap);
+            switchToLabel(label);
+            currentPixmap = pixmap;
+            return;
+        }
+    }
+
+    // 同步加载当前图片
+    QImageReader reader(filePath);
+    reader.setAutoTransform(true);
+    QImage image = reader.read();
+    if (!image.isNull()) {
+        currentPixmap = QPixmap::fromImage(image);
+        updateImage();
+    }
+}
+
+void ImageViewer::displayVideo(int index)
+{
+    QString filePath = mediaPaths[index];
+
+    // Show video widget, hide image stack
+    imageStack->hide();
+    videoWidget->show();
+    playPauseButton->show();
+    playPauseButton->setText("⏸");
+
+    // 使用延迟加载避免 GStreamer 资源竞争
+    pendingVideoPath = filePath;
+    videoPlayTimer->start(100);  // 100ms 延迟
+}
+
+void ImageViewer::playPendingVideo()
+{
+    if (pendingVideoPath.isEmpty()) {
+        return;
+    }
+
+    QString filePath = pendingVideoPath;
+    pendingVideoPath.clear();
+
+    // 设置新的媒体源
+    mediaPlayer->setMedia(QMediaContent(QUrl::fromLocalFile(filePath)));
+
+    // Loop video
+    disconnect(mediaPlayer, &QMediaPlayer::mediaStatusChanged, nullptr, nullptr);
+    connect(mediaPlayer, &QMediaPlayer::mediaStatusChanged, this, [this](QMediaPlayer::MediaStatus status) {
+        if (status == QMediaPlayer::EndOfMedia) {
+            mediaPlayer->setPosition(0);
+            mediaPlayer->play();
+        }
+    }, Qt::UniqueConnection);
+
+    mediaPlayer->play();
+}
+
 bool ImageViewer::isVideoFile(const QString &filePath) const
 {
     QStringList videoExts;
     videoExts << "mp4" << "avi" << "mkv" << "mov" << "wmv" << "flv" << "webm";
-    
+
     QFileInfo fileInfo(filePath);
     QString ext = fileInfo.suffix().toLower();
     return videoExts.contains(ext);
@@ -217,15 +303,33 @@ void ImageViewer::updateImage()
     }
 
     QSize screenSize = size();
-    
+
     // Scale image to fit screen while maintaining aspect ratio
     QPixmap scaledPixmap = currentPixmap.scaled(
         screenSize * 0.9,
         Qt::KeepAspectRatio,
         Qt::SmoothTransformation
     );
-    
-    imageLabel->setPixmap(scaledPixmap);
+
+    QLabel *label = getAvailableLabel();
+    label->setPixmap(scaledPixmap);
+    switchToLabel(label);
+}
+
+QLabel* ImageViewer::getAvailableLabel()
+{
+    // 获取下一个可用的标签（不是当前显示的）
+    int nextIndex = (currentLabelIndex + 1) % imageLabels.size();
+    return imageLabels[nextIndex];
+}
+
+void ImageViewer::switchToLabel(QLabel *label)
+{
+    int index = imageLabels.indexOf(label);
+    if (index >= 0) {
+        imageStack->setCurrentIndex(index);
+        currentLabelIndex = index;
+    }
 }
 
 void ImageViewer::showPrevious()
@@ -233,12 +337,12 @@ void ImageViewer::showPrevious()
     if (mediaPaths.isEmpty()) {
         return;
     }
-    
+
     currentIndex--;
     if (currentIndex < 0) {
         currentIndex = mediaPaths.size() - 1;
     }
-    
+
     loadMedia();
 }
 
@@ -247,12 +351,12 @@ void ImageViewer::showNext()
     if (mediaPaths.isEmpty()) {
         return;
     }
-    
+
     currentIndex++;
     if (currentIndex >= mediaPaths.size()) {
         currentIndex = 0;
     }
-    
+
     loadMedia();
 }
 
@@ -276,27 +380,22 @@ void ImageViewer::keyPressEvent(QKeyEvent *event)
             showNext();
             break;
         case Qt::Key_Home:
-            // Jump to first media
             currentIndex = 0;
             loadMedia();
             break;
         case Qt::Key_End:
-            // Jump to last media
             currentIndex = mediaPaths.size() - 1;
             loadMedia();
             break;
         case Qt::Key_PageUp:
-            // Jump 10 media back
             currentIndex = qMax(0, currentIndex - 10);
             loadMedia();
             break;
         case Qt::Key_PageDown:
-            // Jump 10 media forward
             currentIndex = qMin(mediaPaths.size() - 1, currentIndex + 10);
             loadMedia();
             break;
         case Qt::Key_Space:
-            // Play/Pause video with spacebar
             if (isVideo) {
                 if (mediaPlayer->state() == QMediaPlayer::PlayingState) {
                     mediaPlayer->pause();
@@ -315,17 +414,100 @@ void ImageViewer::keyPressEvent(QKeyEvent *event)
 void ImageViewer::resizeEvent(QResizeEvent *event)
 {
     QDialog::resizeEvent(event);
-    
+
     // Reposition buttons
     closeButton->move(width() - 70, 20);
     prevButton->move(20, height() / 2 - 30);
     nextButton->move(width() - 80, height() / 2 - 30);
     playPauseButton->move(width() / 2 - 30, height() - 100);
-    
+
     // Update media size
     if (isVideo) {
         updateVideo();
-    } else {
-        updateImage();
+    } else if (!currentPixmap.isNull()) {
+        QPixmap scaledPixmap = currentPixmap.scaled(
+            size() * 0.9,
+            Qt::KeepAspectRatio,
+            Qt::SmoothTransformation
+        );
+        QLabel *currentLabel = imageLabels[currentLabelIndex];
+        currentLabel->setPixmap(scaledPixmap);
     }
+
+    // 清除缓存，窗口大小变了需要重新缩放
+    QMutexLocker locker(&cacheMutex);
+    pixmapCache.clear();
+}
+
+// ==================== 预加载相关方法 ====================
+
+void ImageViewer::preloadAdjacent()
+{
+    QSize targetSize = size() * 0.9;
+
+    // 取消之前未完成的预加载
+    for (auto it = preloadWatchers.begin(); it != preloadWatchers.end(); ) {
+        if (it.value() && !it.value()->isFinished()) {
+            it.value()->cancel();
+            disconnect(it.value(), nullptr, nullptr, nullptr);
+            it.value()->waitForFinished();
+            delete it.value();
+            it = preloadWatchers.erase(it);
+        } else {
+            ++it;
+        }
+    }
+
+    // 预加载前后的图片文件
+    for (int offset = -PRELOAD_RANGE; offset <= PRELOAD_RANGE; offset++) {
+        if (offset == 0) continue;
+
+        int preloadIndex = currentIndex + offset;
+        if (preloadIndex < 0 || preloadIndex >= mediaPaths.size()) continue;
+
+        QString filePath = mediaPaths[preloadIndex];
+        if (!isVideoFile(filePath)) {
+            QMutexLocker locker(&cacheMutex);
+            if (pixmapCache.contains(preloadIndex)) {
+                continue;
+            }
+            startPreload(preloadIndex, targetSize);
+        }
+    }
+}
+
+void ImageViewer::startPreload(int index, const QSize &targetSize)
+{
+    if (index < 0 || index >= mediaPaths.size()) return;
+
+    QString filePath = mediaPaths[index];
+
+    QFutureWatcher<QPixmap> *watcher = new QFutureWatcher<QPixmap>();
+    preloadWatchers[index] = watcher;
+
+    connect(watcher, &QFutureWatcher<QPixmap>::finished, this, [this, index, watcher]() {
+        if (watcher->isCanceled()) {
+            return;
+        }
+        QPixmap pixmap = watcher->result();
+        if (!pixmap.isNull()) {
+            QMutexLocker locker(&cacheMutex);
+            pixmapCache[index] = pixmap;
+        }
+        preloadWatchers.remove(index);
+        watcher->deleteLater();
+    });
+
+    QFuture<QPixmap> future = QtConcurrent::run([filePath, targetSize]() {
+        QImageReader reader(filePath);
+        reader.setAutoTransform(true);
+        QImage image = reader.read();
+        if (!image.isNull()) {
+            QPixmap pixmap = QPixmap::fromImage(image);
+            return pixmap.scaled(targetSize, Qt::KeepAspectRatio, Qt::SmoothTransformation);
+        }
+        return QPixmap();
+    });
+
+    watcher->setFuture(future);
 }
--- imageviewer.h
+++ imageviewer.h
@@ -8,6 +8,12 @@
 #include <QStringList>
 #include <QMediaPlayer>
 #include <QVideoWidget>
+#include <QMap>
+#include <QFutureWatcher>
+#include <QImage>
+#include <QStackedWidget>
+#include <QMutex>
+#include <QTimer>
 
 class ImageViewer : public QDialog
 {
@@ -15,6 +21,7 @@ class ImageViewer : public QDialog
 
 public:
     explicit ImageViewer(const QString &filePath, const QStringList &allPaths, QWidget *parent = nullptr);
+    ~ImageViewer();
 
 protected:
     void keyPressEvent(QKeyEvent *event) override;
@@ -23,6 +30,7 @@ protected:
 private slots:
     void showPrevious();
     void showNext();
+    void playPendingVideo();
 
 private:
     void loadMedia();
@@ -30,19 +38,42 @@ private:
     void updateImage();
     void updateVideo();
     bool isVideoFile(const QString &filePath) const;
+    void displayImage(int index);
+    void displayVideo(int index);
 
-    QLabel *imageLabel;
+    // 预加载相关
+    void preloadAdjacent();
+    void startPreload(int index, const QSize &targetSize);
+
+    // 双缓冲相关
+    QLabel* getAvailableLabel();
+    void switchToLabel(QLabel *label);
+
+    QStackedWidget *imageStack;
+    QList<QLabel*> imageLabels;
+    int currentLabelIndex;
+
     QVideoWidget *videoWidget;
     QMediaPlayer *mediaPlayer;
     QPushButton *prevButton;
     QPushButton *nextButton;
     QPushButton *closeButton;
     QPushButton *playPauseButton;
-    
+
     QStringList mediaPaths;
     int currentIndex;
     QPixmap currentPixmap;
     bool isVideo;
+
+    // 视频延迟播放
+    QString pendingVideoPath;
+    QTimer *videoPlayTimer;
+
+    // 预加载缓存
+    static const int PRELOAD_RANGE = 3;
+    QMap<int, QPixmap> pixmapCache;
+    QMap<int, QFutureWatcher<QPixmap>*> preloadWatchers;
+    QMutex cacheMutex;
 };
 
 #endif // IMAGEVIEWER_H
